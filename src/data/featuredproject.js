c66cdd74cfec1055d27129b08cc3676341574402
Benjamin <benjamin.morales3@mail.udp.cl>
2025-08-12 22:05:21 -0400

    progress

---
+++ src/data/featuredproject.js
@@ -0,0 +1,24 @@
+export const featuredProject = {
+    title: {
+        es: "Primer Freelance: Organizate.cl",
+        en: "First Freelance: Organizate.cl",
+        de: "Erster Freelance-Auftrag: Organizate.cl"
+    },
+    subtitle: {
+    es: "E-commerce de productos de organización",
+    en: "E-commerce for organization products",
+    de: "E-Commerce für Organisationsprodukte"
+    },
+    description: {
+        es: "Página creada para un cliente en particular con actualizaciones mensuales y actualmente en línea. Es posible comprar de manera online mediante Mercado Pago.",
+        en: "Website created for a specific client with monthly updates and currently online. Online purchases can be made through Mercado Pago.",
+        de: "Website für einen bestimmten Kunden erstellt, mit monatlichen Aktualisierungen und derzeit online. Online-Käufe sind über Mercado Pago möglich."
+    },
+    image: "p12.PNG",
+    repo: "",
+    youtube: "",
+    link: "https://www.organizzarte.cl",
+    tags: ["nextjs", "tailwind", "nodejs", "mongodb"],
+    paid: true,
+    category: "fullstack",
+};
